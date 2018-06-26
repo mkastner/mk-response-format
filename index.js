55b05d567548125dc20caff7a566d48d91ecdc00
Michael Kastner <kastner@galt.de>
2018-06-26 10:57:49 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+function getAcceptType(req) {
+
+  if(req.params.ext === 'json' || /application\/json/.test(req.get('accept'))) {
+    return 'json'; 
+  } else if(req.params.ext === 'html' || /text\/html/.test(req.get('accept'))) {
+    return 'html'; 
+  } else if(req.params.ext === 'xml' || /application\/xml/.test(req.get('accept'))) { 
+    return 'xml'; 
+  } else {
+    return 'html';
+  }
+}
+
+const ResTypes = {
+  json: 'application/json',
+  xml: 'text/xml',
+  html: 'text/html'
+};
+
+// data is optional and only used in adapters 
+module.exports = function responseFormat(cb, data) {
+  
+  return (req, res) => {
+    
+    let acceptType = getAcceptType(req);
+
+    cb(req, res, {
+      use(useType, handler) {
+
+        if (useType === acceptType) {
+          let resType = ResTypes[useType];
+          res.setHeader('Content-Type', resType);
+          // finally execute the handler
+          // data is optional and only used in adapters 
+          handler(res, data); 
+        }
+        return this;
+      } 
+    });
+  };
+};
